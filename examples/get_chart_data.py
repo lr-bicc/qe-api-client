70207ad3335a9a707398e4990d35fc00b2bdd169
Rumen Vasilev <rumen.vasilev@gmail.com>
2025-05-30 18:36:33 +0200

    Example "get_chart_data" reworked.

--- examples/get_chart_data.py
+++ examples/get_chart_data.py
@@ -17,6 +17,12 @@ table_1_id = sample_app[9]
 # Get pivot table id
 pivot_table_1_id = sample_app[12]
 
+# Get straight table id
+sn_table_1_id = sample_app[15]
+
+# Get new pivot table id
+sn_pivot_table_1_id = sample_app[18]
+
 # Retrieves the content of the table as dataframe
 table_data = conn.get_chart_data(app_handle, table_1_id)
 print("Table content:")
@@ -29,6 +35,20 @@ pivot_table_data = conn.get_chart_data(app_handle, pivot_table_1_id)
 print("Pivot table content:")
 print("=================================")
 print(pivot_table_data.to_string())
+print("\n")
+
+# Retrieves the content of the straight table as dataframe
+sn_table_data = conn.get_chart_data(app_handle, sn_table_1_id)
+print("Straight table content:")
+print("=================================")
+print(sn_table_data.to_string())
+print("\n")
+
+# Retrieves the content of the new pivot table as dataframe
+sn_pivot_table_data = conn.get_chart_data(app_handle, sn_pivot_table_1_id)
+print("New pivot table content:")
+print("=================================")
+print(sn_pivot_table_data.to_string())
 print("\n")
 
 # close engine connection
--- test/test.py
+++ test/test.py
@@ -2,42 +2,35 @@ from qe_api_client.engine import QixEngine
 import math
 import pandas as pd
 
-# url = 'ws://localhost:4848/app'
-# qixe = QixEngine(url=url)
+url = 'ws://localhost:4848/app'
+qixe = QixEngine(url=url)
 
-url = "lr-analytics-test.lr-netz.local"
-user_directory = "LR"
-user_id = "!QlikSense"
-qlik_certs_path = "C:/LocalUserData/Certificates/Sense TEST"
-ca_certs = qlik_certs_path + "/root.pem"
-certfile = qlik_certs_path + "/client.pem"
-keyfile = qlik_certs_path + "/client_key.pem"
-qixe = QixEngine(url, user_directory, user_id, ca_certs, certfile, keyfile)
+# url = "lr-analytics-test.lr-netz.local"
+# user_directory = "LR"
+# user_id = "!QlikSense"
+# qlik_certs_path = "C:/LocalUserData/Certificates/Sense TEST"
+# ca_certs = qlik_certs_path + "/root.pem"
+# certfile = qlik_certs_path + "/client.pem"
+# keyfile = qlik_certs_path + "/client_key.pem"
+# qixe = QixEngine(url, user_directory, user_id, ca_certs, certfile, keyfile)
 
 # App ID holen
-doc_id = "0c6a91a3-4dc0-490e-ae0f-41391b39c2ec" # Bonus Competitions
-# doc_id = "f9e79d92-652b-4ba8-8487-84e2825b71c5"     # Sales KPI
-# doc_id = "Test.qvf"
+# app_id = "0c6a91a3-4dc0-490e-ae0f-41391b39c2ec" # Bonus Competitions
+# app_id = "f9e79d92-652b-4ba8-8487-84e2825b71c5"     # Sales KPI
+# app_id = "3b9ef434-f4e9-4310-9cef-1347502bc39d"     # Stocks
+# app_id = "0a64346c-da25-4fd5-b1a7-e3d897d270e3"     # Sales & Stocks
+app_id = "Test.qvf"
 
 # App öffnen
-opened_doc = qixe.ega.open_doc(doc_id)
-print(opened_doc)
+opened_app = qixe.ega.open_doc(app_id)
 
-doc_handle = qixe.get_handle(opened_doc)
+app_handle = qixe.get_handle(opened_app)
 
-# # Lineage-Daten aus der API holen
-# lineage = qixe.eaa.get_lineage(doc_handle)
-# print(lineage)
-#
-# # Erstelle den DataFrame und fülle fehlende Werte mit ""
-# df = pd.DataFrame(lineage)  #.fillna("")
-# df = df[(df["qDiscriminator"].notna()) | (df["qStatement"].notna())].fillna("")
-# # df = df.reindex(columns=["qDiscriminator", "qStatement"]).fillna("")
+# qixe.select_in_field(app_handle=app_handle, field_name="TopicBox_ABC_Analysis", list_of_values=["ABC Analysis", "Detailed stock information", "Avg. sales per week + range of coverage", "Sales per month", "Sales per week"])
 
-df = qixe.get_app_lineage_info(doc_handle)
-
-print(df)
-
+df = qixe.get_chart_data(app_handle=app_handle, obj_id="xPpxvy")
+print(df.to_string())
+df.to_csv('C:/Users/R.Vasilev/OneDrive - LR/Desktop/out.csv', index=False)
 
 # Websocket-Verbindung schließen
 QixEngine.disconnect(qixe)
